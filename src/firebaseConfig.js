dfd2dda109b4d317d779179c187414eacc3c3091
Sanjay Sharma <forusanjay007@gmail.com>
2023-03-27 14:54:27 +0530

    Add files via upload

---
+++ src/firebaseConfig.js
@@ -0,0 +1,15 @@
+import {initializeApp}from 'firebase/app';
+
+ const firebaseConfig = {
+  apiKey: "AIzaSyAH4Q5xoVwSvli6gg64yJDyFrRa9KtvIF8",
+  authDomain: "test-app-ed84a.firebaseapp.com",
+  projectId: "test-app-ed84a",
+  storageBucket: "test-app-ed84a.appspot.com",
+  messagingSenderId: "492907454224",
+  appId: "1:492907454224:web:6425bfbcb8bcda7ce44f95"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
